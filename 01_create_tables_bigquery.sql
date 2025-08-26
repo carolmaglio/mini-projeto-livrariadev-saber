4d8cadc49d33d30e522ed2e78c171df90284091f
Caroline Maglio <129905279+carolmaglio@users.noreply.github.com>
2025-08-26 17:47:20 -0300

    Update 01_create_tables_bigquery.sql
    create tables Clientes, Produtos and Vendas

--- 01_create_tables_bigquery.sql
+++ 01_create_tables_bigquery.sql
@@ -1,1 +1,24 @@
+-- tabela Clientes
+CREATE OR REPLACE TABLE `t1engenhariadados.LivrariaDevSaber_CarolMaglio.Clientes` (
+  ID_Cliente INT64,
+  Nome_Cliente STRING,
+  Email_Cliente STRING,
+  Estado_Cliente STRING,
+);
 
+-- tabela Produtos
+CREATE OR REPLACE TABLE `t1engenhariadados.LivrariaDevSaber_CarolMaglio.Produtos` (
+  ID_Produto INT64,
+  Nome_Produto STRING,
+  Categoria_Produto STRING,
+  Preco_Produto NUMERIC 
+);
+
+-- tabela Vendas
+CREATE OR REPLACE TABLE `t1engenhariadados.LivrariaDevSaber_CarolMaglio.Vendas` (
+  ID_Venda INT64,
+  ID_Cliente INT64,
+  ID_Produto INT64,
+  Data_Venda DATE,
+  Quantidade INT64
+);
